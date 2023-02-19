e6b2a1ddc53ad3dac376a04e9196006da85523c6
varsha-vijayan <117884865+varsha-vijayan@users.noreply.github.com>
2023-02-19 21:03:22 +0530

    Create line_by_line_store_list.py

---
+++ line_by_line_store_list.py
@@ -0,0 +1,8 @@
+def read_file(file_name): 
+with open (file_name) as f: 
+content_list=f.readlines( ) 
+print (content_list) 
+read_file(‗file1.txt‘) 
+
+INPUT TEXT FILE: file1.txt 
+Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. Python's simple, easy to learn syntax emphasizes readability and therefore reduces the cost of program maintenance. 
